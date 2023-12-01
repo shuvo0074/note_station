b71fd201cee1ea519ffeb4548a6ee0f7ca55327a
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2023-12-01 17:11:58 +0600

    add required assets

---
+++ app/assets/index.js
@@ -0,0 +1,10 @@
+const home = {
+  addNote: require('./home/addNote.png'),
+  screenBg: require('./home/screenBg.png'),
+  errorBg: require('./home/errorBg.png'),
+  icCorrect: require('./home/icCorrect.png'),
+};
+
+export const assets = {
+  home
+};
